6f29476e84fd1f77e29dd01ef5a782e3ddc398d4
Mark <markhegarty.audio@gmail.com>
2022-02-03 16:55:02 -0500

    project 05 portfolio, did all the html, a bunch of styling, and some JS

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// const canvas = document.querySelector("canvas")
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
+
+// console.log(canvas);
+
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+    console.log('callback - particles-js config loaded');
+  });
+
+  
