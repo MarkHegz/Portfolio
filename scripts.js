3dbcbc8359f378a90602172f42dd1288b1176c68
Mark <markhegarty.audio@gmail.com>
2022-02-04 01:35:26 -0500

    added media queries, still trying to implement hamburger nav

--- scripts.js
+++ scripts.js
@@ -4,8 +4,26 @@
 
 // console.log(canvas);
 
-particlesJS.load('particles-js', 'assets/particles.json', function() {
-    console.log('callback - particles-js config loaded');
-  });
+// particlesJS.load('particles-js', 'assets/particles.json', function() {
+//     console.log('callback - particles-js config loaded');
+//   });
 
-  
+  const menu = document.querySelector(".menu");
+  const hamburger = document.querySelector(".hamburger");
+  const closeMenu = document.querySelector(".closeMenu");
+
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    menu.classList.toggle("active");
+    console.log(hamburger)
+  })
+  // closeMenu.addEventListener("click", close);
+
+  // function show() {
+  //   menu.style.display = "flex";
+  //   menu.style.top = "0";
+  // }
+
+  // function close() {
+  //   menu.style.top = "-100%";
+  // }
