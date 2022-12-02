20a8426e6b1f9b63423fdef53612cf6348c947bf
Mark <markhegarty.audio@gmail.com>
2022-12-02 08:56:35 -0500

    added some JS

--- scripts.js
+++ scripts.js
@@ -1,12 +1,12 @@
-const canvas = document.querySelector("canvas")
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+// const canvas = document.querySelector("canvas")
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
 
-console.log(canvas);
+// console.log(canvas);
 
-particlesJS.load('particles-js', 'assets/particles.json', function() {
-    console.log('callback - particles-js config loaded');
-  });
+// particlesJS.load('particles-js', 'assets/particles.json', function() {
+//     console.log('callback - particles-js config loaded');
+//   });
 
 
 
